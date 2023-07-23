beb44ffb86d1b08ff1a23e3b77d35e2cfed006d7
quangdung861 <quangdung861@gmail.com>
2023-07-23 11:16:18 +0700

    First commit

--- src/components/Filters/index.js
+++ src/components/Filters/index.js
@@ -1,86 +1,50 @@
-import { useEffect, useState } from "react";
-import { Col, Row, Input, Typography, Radio, Select, Tag } from "antd";
-import { useDispatch } from "react-redux";
-// import {
-//   prioritiesFilterChange,
-//   searchFilterChange,
-//   statusFilterChange,
-// } from "../../redux/actions";
-
-import filtersSlice from "./filtersSlice";
+import { Col, Row, Input, Typography, Radio, Select, Tag } from 'antd';
 
 const { Search } = Input;
 
 export default function Filters() {
-  const dispatch = useDispatch();
-  const [searchText, setSearchText] = useState("");
-  const [filterStatus, setFilterStatus] = useState("All");
-
-  const handleSearchTextChange = (e) => {
-    setSearchText(e.target.value);
-    dispatch(filtersSlice.actions.searchText(e.target.value));
-  };
-
-  const handleStatusChange = (e) => {
-    setFilterStatus(e.target.value);
-    dispatch(filtersSlice.actions.statusFilterChange(e.target.value));
-  };
-
-  const handlePrioriesChange = (e) => {
-    console.log(e);
-    dispatch(filtersSlice.actions.prioritiesFilterChange(e));
-  };
-
   return (
-    <Row justify="center">
+    <Row justify='center'>
       <Col span={24}>
         <Typography.Paragraph
-          style={{ fontWeight: "bold", marginBottom: 3, marginTop: 10 }}
+          style={{ fontWeight: 'bold', marginBottom: 3, marginTop: 10 }}
         >
           Search
         </Typography.Paragraph>
-        <Search
-          placeholder="input search text"
-          value={searchText}
-          onChange={(e) => handleSearchTextChange(e)}
-        />
+        <Search placeholder='input search text' />
       </Col>
       <Col sm={24}>
         <Typography.Paragraph
-          style={{ fontWeight: "bold", marginBottom: 3, marginTop: 10 }}
+          style={{ fontWeight: 'bold', marginBottom: 3, marginTop: 10 }}
         >
           Filter By Status
         </Typography.Paragraph>
-        <Radio.Group
-          value={filterStatus}
-          onChange={(e) => handleStatusChange(e)}
-        >
-          <Radio value="All">All</Radio>
-          <Radio value="Completed">Completed</Radio>
-          <Radio value="Todo">To do</Radio>
+        <Radio.Group>
+          <Radio value='All'>All</Radio>
+          <Radio value='Completed'>Completed</Radio>
+          <Radio value='Todo'>To do</Radio>
         </Radio.Group>
       </Col>
       <Col sm={24}>
         <Typography.Paragraph
-          style={{ fontWeight: "bold", marginBottom: 3, marginTop: 10 }}
+          style={{ fontWeight: 'bold', marginBottom: 3, marginTop: 10 }}
         >
           Filter By Priority
         </Typography.Paragraph>
         <Select
-          mode="multiple"
+          mode='multiple'
           allowClear
-          placeholder="Please select"
-          style={{ width: "100%" }}
-          onChange={(e) => handlePrioriesChange(e)}
+          placeholder='Please select'
+          style={{ width: '100%' }}
         >
-          <Select.Option value="High" label="High">
-            <Tag color="red">High</Tag>
+          <Select.Option value='High' label='High'>
+            <Tag color='red'>High</Tag>
           </Select.Option>
-          <Select.Option value="Medium" label="Medium">
-            <Tag color="blue">Medium</Tag>
+          <Select.Option value='Medium' label='Medium'>
+            <Tag color='blue'>Medium</Tag>
           </Select.Option>
-          <Select.Option value="Low" label="Low">
-            <Tag color="gray">Low</Tag>
+          <Select.Option value='Low' label='Low'>
+            <Tag color='gray'>Low</Tag>
           </Select.Option>
         </Select>
       </Col>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import "./index.css";
-import App from "./App";
-import store from "./redux/store";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById('root'));
